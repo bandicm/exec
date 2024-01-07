cd295a93b678528b6b0d79c69a43c0209fd88a77
marcelb <marcelb@bitelex.ml>
2024-01-07 21:33:04 +0100

    Readme, comments

--- test/test.cpp
+++ test/test.cpp
@@ -3,13 +3,12 @@
 #include "../lib/exec.hpp"
 
 using namespace std;
+using namespace marcelb;
 
 int main() {
 
     string out = exec("ls -lha");
-
     cout << out << endl;
     
-
     return 0;
 }
